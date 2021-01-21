ed439826c110cc6fb5fa6127dd08e18375b6a4d5
SETAGI <tamayo0711@hotmail.com>
2021-01-21 10:24:15 -0500

    Multiplication and division methods were created

--- src/data/Calculator.java
+++ src/data/Calculator.java
@@ -2,4 +2,20 @@ package data;
 
 public class Calculator {
 
+    private double number1;
+    private double number2;
+
+    public Calculator(double number1, double number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public double multiplication(){
+        return this.number1 * this.number2;
+    }
+
+    public double division(){
+        return this.number1 / this.number2;
+    }
+
 }
